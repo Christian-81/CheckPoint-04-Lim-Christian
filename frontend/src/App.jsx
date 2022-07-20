b5bf9ccc772d0e15554dbc137a38b4eadd274cca
limchristian81@gmail.com <limchristian81@gmail.com>
2022-07-20 22:26:01 +0200

    CREATION routes and add pictures and socials logos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import Home from "@pages/Home";
+import { Routes, Route } from "react-router-dom";
+
 
 import "./App.css"
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<Carte />} />
+        <Route path="/carte/:id" element={<CarteDetail />} />
+        <Route path="/menu-du-jour/" element={<MenuDuJour />} />
+        <Route path="/equipe/" element={<Equipe />} />
+      </Routes>
       <Home />
     </div>
   );
---
+++ frontend/src/pages/MenuDuJour.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MenuDuJour = () => {
+  return (
+    <div>Menu-Du-Jour</div>
+  )
+}
+
+export default MenuDuJour
